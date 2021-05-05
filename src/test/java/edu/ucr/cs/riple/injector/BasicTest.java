90871c7658e83e23cc7b3cf01831501f7a9d6f26
nimakarimipour <karimipour.nima@gmail.com>
2021-05-05 15:34:05 -0700

    updated tests

--- src/test/java/edu/ucr/cs/riple/injector/BasicTest.java
+++ src/test/java/edu/ucr/cs/riple/injector/BasicTest.java
@@ -848,6 +848,95 @@ public class BasicTest {
   }
 
   @Test
+  public void save_imports_asterisk() {
+    String rootName = "save_imports_asterisk";
+    new InjectorTestHelper()
+            .setRootPath(System.getProperty("user.dir") + "/tests/" + rootName)
+            .addInput(
+                    "Super.java",
+                    "package com.uber;",
+                    "import static com.ibm.wala.types.A;",
+                    "import static com.ibm.wala.types.B;",
+                    "import static com.ibm.wala.types.C;",
+                    "import static com.ibm.wala.types.D;",
+                    "import static com.ibm.wala.types.E;",
+                    "import static com.ibm.wala.types.F;",
+                    "import static com.ibm.wala.types.G;",
+                    "import static com.ibm.wala.types.H;",
+                    "import static com.ibm.wala.types.I;",
+                    "import static com.ibm.wala.types.J;",
+                    "import static com.ibm.wala.types.K;",
+                    "import static com.ibm.wala.types.L;",
+                    "import com.ibm.wala.util.A;",
+                    "import com.ibm.wala.util.B;",
+                    "import com.ibm.wala.util.C;",
+                    "import com.ibm.wala.util.D;",
+                    "import com.ibm.wala.util.E;",
+                    "import com.ibm.wala.util.F;",
+                    "import com.ibm.wala.util.G;",
+                    "import com.ibm.wala.util.H;",
+                    "import com.ibm.wala.util.I;",
+                    "import com.ibm.wala.util.J;",
+                    "import com.ibm.wala.util.K;",
+                    "import com.ibm.wala.util.L;",
+                    "import com.ibm.wala.util.M;",
+                    "import com.ibm.wala.util.N;",
+                    "public class Super {",
+                    "   Object test() {",
+                    "       return new Object();",
+                    "   }",
+                    "}")
+            .expectOutput(
+                    "Super.java",
+                    "package com.uber;",
+                    "import static com.ibm.wala.types.A;",
+                    "import static com.ibm.wala.types.B;",
+                    "import static com.ibm.wala.types.C;",
+                    "import static com.ibm.wala.types.D;",
+                    "import static com.ibm.wala.types.E;",
+                    "import static com.ibm.wala.types.F;",
+                    "import static com.ibm.wala.types.G;",
+                    "import static com.ibm.wala.types.H;",
+                    "import static com.ibm.wala.types.I;",
+                    "import static com.ibm.wala.types.J;",
+                    "import static com.ibm.wala.types.K;",
+                    "import static com.ibm.wala.types.L;",
+                    "import com.ibm.wala.util.A;",
+                    "import com.ibm.wala.util.B;",
+                    "import com.ibm.wala.util.C;",
+                    "import com.ibm.wala.util.D;",
+                    "import com.ibm.wala.util.E;",
+                    "import com.ibm.wala.util.F;",
+                    "import com.ibm.wala.util.G;",
+                    "import com.ibm.wala.util.H;",
+                    "import com.ibm.wala.util.I;",
+                    "import com.ibm.wala.util.J;",
+                    "import com.ibm.wala.util.K;",
+                    "import com.ibm.wala.util.L;",
+                    "import com.ibm.wala.util.M;",
+                    "import com.ibm.wala.util.N;",
+                    "import javax.annotation.Nullable;",
+                    "public class Super {",
+                    "   @Nullable",
+                    "   Object test() {",
+                    "       return new Object();",
+                    "   }",
+                    "}")
+            .addFixes(
+                    new Fix(
+                            "javax.annotation.Nullable",
+                            "test()",
+                            "",
+                            "METHOD_RETURN",
+                            "com.uber.Super",
+                            "com.uber",
+                            "Super.java",
+                            "true"))
+            .start();
+  }
+
+
+  @Test
   public void remove_redundant_new_keyword() {
     String rootName = "remove_redundant_new_keyword";
     new InjectorTestHelper()
---
+++ tests/save_imports_asterisk/src/Super.java
@@ -0,0 +1,32 @@
+package com.uber;
+import static com.ibm.wala.types.A;
+import static com.ibm.wala.types.B;
+import static com.ibm.wala.types.C;
+import static com.ibm.wala.types.D;
+import static com.ibm.wala.types.E;
+import static com.ibm.wala.types.F;
+import static com.ibm.wala.types.G;
+import static com.ibm.wala.types.H;
+import static com.ibm.wala.types.I;
+import static com.ibm.wala.types.J;
+import static com.ibm.wala.types.K;
+import static com.ibm.wala.types.L;
+import com.ibm.wala.util.A;
+import com.ibm.wala.util.B;
+import com.ibm.wala.util.C;
+import com.ibm.wala.util.D;
+import com.ibm.wala.util.E;
+import com.ibm.wala.util.F;
+import com.ibm.wala.util.G;
+import com.ibm.wala.util.H;
+import com.ibm.wala.util.I;
+import com.ibm.wala.util.J;
+import com.ibm.wala.util.K;
+import com.ibm.wala.util.L;
+import com.ibm.wala.util.M;
+import com.ibm.wala.util.N;
+public class Super {
+   Object test() {
+       return new Object();
+   }
+}
