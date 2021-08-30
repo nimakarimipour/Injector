7a16bc1ab3e285ea4c6ed28092ab35db24b80cf7
nimakarimipour <karimipour.nima@gmail.com>
2021-08-30 12:20:17 -0700

    update

--- src/main/java/edu/ucr/cs/riple/injector/util/Utility.java
+++ src/main/java/edu/ucr/cs/riple/injector/util/Utility.java
@@ -3,7 +3,6 @@ package edu.ucr.cs.riple.injector.util;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.expr.AnnotationExpr;
-
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.HashSet;
@@ -13,15 +12,19 @@ import java.util.stream.Collectors;
 
 public class Utility {
 
-    public static Set<String> findAllAnnots(Path path) {
-        Set<String> ans = new HashSet<>();
-        try {
-            CompilationUnit tree = StaticJavaParser.parse(path);
-            List<AnnotationExpr> annots = tree.findAll(AnnotationExpr.class);
-            ans = annots.stream().map(annotationExpr -> annotationExpr.getName().asString()).collect(Collectors.toSet());
-        } catch (IOException e) {
-            System.out.println("Exception happened: " + e.getMessage());
-        }
-        return ans;
+  public static Set<String> findAllAnnots(Path path) {
+    Set<String> ans = new HashSet<>();
+    try {
+      CompilationUnit tree = StaticJavaParser.parse(path);
+      List<AnnotationExpr> annots = tree.findAll(AnnotationExpr.class);
+      ans =
+          annots
+              .stream()
+              .map(annotationExpr -> annotationExpr.getName().asString())
+              .collect(Collectors.toSet());
+    } catch (IOException e) {
+      System.out.println("Exception happened: " + e.getMessage());
     }
+    return ans;
+  }
 }
