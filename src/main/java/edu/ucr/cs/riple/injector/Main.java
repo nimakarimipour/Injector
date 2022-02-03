87cda5b3258e1d33a32a2480334c2999a7be5ae7
nimakarimipour <karimipour.nima@gmail.com>
2022-02-03 14:59:14 -0800

    Update

--- src/main/java/edu/ucr/cs/riple/injector/Main.java
+++ src/main/java/edu/ucr/cs/riple/injector/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
   public static void main(String[] args) {
     Injector.LOG = false;
-    String path = args[1];
+    String path = args[0];
+    System.out.println("Received in jar: " + path);
     Injector injector =
         new Injector.InjectorBuilder().setMode(Injector.MODE.BATCH).keepStyle(true).build();
     injector.start(new WorkListBuilder(path).getWorkLists());
